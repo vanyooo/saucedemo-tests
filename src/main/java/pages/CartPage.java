10e1010cdb7e83f3c3c185a8a868cb4962dff632
Ivan <zimin.ivann.1998@gmail.com>
2026-03-19 16:47:49 +0300

    add allure.properties

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -4,9 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class CartPage extends BasePage {
 
     public CartPage(WebDriver driver) {
